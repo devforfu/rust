946822c88400f6ef1dad133d34d47ad5b4627618
devforfu <developer.z@outlook.com>
2021-05-28 13:03:04 +0200

    State pattern

---
+++ oop/src/blog.rs
@@ -0,0 +1,112 @@
+mod post {
+    pub struct Post {
+        state: Option<Box<dyn State>>,
+        content: String,
+    }
+
+    impl Post {
+        pub fn new() -> Post {
+            Post {
+                state: Some(Box::new(Draft {})),
+                content: String::new(),
+            }
+        }
+
+        pub fn content(&self) -> &str {
+            self.state.as_ref().unwrap().content(self)
+        }
+
+        pub fn add_text(&mut self, text: &str) {
+            self.content.push_str(text);
+        }
+
+        pub fn request_review(&mut self) {
+            if let Some(s) = self.state.take() {
+                self.state = Some(s.request_review())
+            }
+        }
+
+        pub fn approve(&mut self) {
+            if let Some(s) = self.state.take() {
+                self.state = Some(s.approve())
+            }
+        }
+    }
+
+    trait State {
+        fn request_review(self: Box<Self>) -> Box<dyn State>;
+        fn approve(self: Box<Self>) -> Box<dyn State>;
+        fn content<'a>(&self, post: &'a Post) -> &'a str {
+            ""
+        }
+    }
+
+    struct Draft {}
+
+    impl State for Draft {
+        fn request_review(self: Box<Self>) -> Box<dyn State> {
+            Box::new(PendingReview {})
+        }
+
+        fn approve(self: Box<Self>) -> Box<dyn State> {
+            self
+        }
+    }
+
+    struct PendingReview {}
+
+    impl State for PendingReview {
+        fn request_review(self: Box<Self>) -> Box<dyn State> {
+            self
+        }
+
+        fn approve(self: Box<Self>) -> Box<dyn State> {
+            Box::new(Published {})
+        }
+    }
+
+    struct Published {}
+
+    impl State for Published {
+        fn request_review(self: Box<Self>) -> Box<dyn State> {
+            self
+        }
+
+        fn approve(self: Box<Self>) -> Box<dyn State> {
+            self
+        }
+
+        fn content<'a>(&self, post: &'a Post) -> &'a str {
+            &post.content
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::post::{Post};
+
+    #[test]
+    fn test_new_post_created() {
+        let post = Post::new();
+
+        assert_eq!(post.content(), "");
+    }
+
+    #[test]
+    fn test_post_states() {
+        let mut post = Post::new();
+
+        post.add_text("Some text");
+        assert_eq!(post.content(), "");
+
+        post.approve();
+        assert_eq!(post.content(), "");
+
+        post.request_review();
+        assert_eq!(post.content(), "");
+
+        post.approve();
+        assert_eq!(post.content(), "Some text");
+    }
+}
