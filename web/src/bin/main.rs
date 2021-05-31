d5b60c200fe9b114cd1998d6d428031aee1b6987
devforfu <developer.z@outlook.com>
2021-05-31 23:17:33 +0200

    Multi-worker server

--- web/src/bin/main.rs
+++ web/src/bin/main.rs
@@ -1,18 +1,20 @@
-use std::{fs, env};
+use std::{fs, env, thread};
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
-
-mod server;
+use std::time::Duration;
+use web::{Config, ThreadPool};
 
 fn main() {
-    let config = server::Config::default();
-
+    let config = Config::default();
     let listener = TcpListener::bind(config.address()).unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -27,8 +29,12 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep= b"GET /sleep HTTP/1.1\r\n";
 
     let (status, template) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "hello")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404")
--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,6 +1,7 @@
 use std::{env, thread};
 use std::net::TcpListener;
 use std::thread::JoinHandle;
+use std::sync::{mpsc, Arc, Mutex};
 
 static HOST_ENV: &str = "RUST_HOST";
 static PORT_ENV: &str = "RUST_PORT";
@@ -46,6 +47,65 @@ pub fn listen(config: &Config) -> JoinHandle<bool> {
     handle
 }
 
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
+    }
+}
+
+pub struct Worker {
+    id: usize,
+    thread: JoinHandle<()>,
+}
+
+impl Worker {
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {} got a job; executing.", id);
+
+            job();
+        });
+
+        Worker { id, thread }
+    }
+}
 
 #[cfg(test)]
 mod tests {
