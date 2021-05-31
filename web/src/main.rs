1342bc84ad8b22e2b8cd11f834eafb4bffb7928a
devforfu <developer.z@outlook.com>
2021-05-31 20:42:37 +0200

    Simple HTML response

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::{fs, env};
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
 mod server;
 
 fn main() {
@@ -7,8 +10,39 @@ fn main() {
     let listener = TcpListener::bind(config.address()).unwrap();
 
     for stream in listener.incoming() {
-        let _ = stream.unwrap();
+        let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn read_template(name: &str) -> String {
+    let root = String::from(env::current_dir().unwrap().to_str().unwrap());
+    let path = format!("{}/templates/{}.html", root, name);
+    fs::read_to_string(path).unwrap()
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status, template) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404")
+    };
+
+    let contents = read_template(template);
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status,
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
