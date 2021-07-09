0ab66b513b6ba0b15b1bf2417fd8311af2eea362
devforfu <developer.z@outlook.com>
2021-07-09 20:41:51 +0200

    anyhow lib

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,3 +1,7 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use anyhow::Result;
 use structopt::StructOpt;
 
 /// Search for a pattern in a file and display the lines that contain it.
@@ -8,4 +12,21 @@ pub struct Cli {
     /// The path to the file to read
     #[structopt(parse(from_os_str))]
     pub path: std::path::PathBuf,
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path> {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_lines() {
+        // read_lines()
+    }
+
 }
