d4470ec53c669b9b2550633371b8d9502542e9dd
devforfu <developer.z@outlook.com>
2021-07-11 22:52:24 +0200

    Reading lines from file

--- minigrep/src/bin/main.rs
+++ minigrep/src/bin/main.rs
@@ -1,6 +1,12 @@
+use anyhow::Result;
 use structopt::StructOpt;
-use minigrep::Cli;
+use minigrep::{Cli, read_lines};
 
-fn main() {
-    println!("{:?}", Cli::from_args());
+fn main() -> Result<()> {
+    let args: Cli = Cli::from_args();
+    let lines = read_lines(&args.path)?;
+    for (i, line) in lines.enumerate() {
+        println!("{}: {}", i, line?);
+    }
+    Ok(())
 }
--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,7 +1,6 @@
+use anyhow::{Result, Context};
 use std::fs::File;
 use std::io::{self, BufRead};
-use std::path::Path;
-use anyhow::Result;
 use structopt::StructOpt;
 
 /// Search for a pattern in a file and display the lines that contain it.
@@ -10,23 +9,47 @@ pub struct Cli {
     /// The pattern to look for
     pub pattern: String,
     /// The path to the file to read
-    #[structopt(parse(from_os_str))]
-    pub path: std::path::PathBuf,
+    // #[structopt(parse(from_os_str))]
+    pub path: String,
 }
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path> {
-    let file = File::open(filename)?;
+pub fn read_lines(filename: &str) -> Result<io::Lines<io::BufReader<File>>>
+{
+    let file = File::open(filename).with_context(|| {
+        format!("could not read file `{}`", filename)
+    })?;
     Ok(io::BufReader::new(file).lines())
 }
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::sync::Once;
+    use std::io::Write;
+
+    static INIT: Once = Once::new();
+    static TEST_FILE: &str = "/tmp/test.txt";
+
+    fn init() {
+        INIT.call_once(|| {
+            create_test_file("first\nsecond\nthird");
+        });
+    }
+
+    fn create_test_file(content: &str) {
+        let mut file = File::create(TEST_FILE).unwrap();
+        file.write_all(content.as_bytes()).unwrap();
+    }
 
     #[test]
-    fn test_read_lines() {
-        // read_lines()
+    fn test_read_lines() -> Result<()> {
+        init();
+
+        let lines: Vec<String> = read_lines(TEST_FILE)?.map(|line| line.unwrap()).collect();
+
+        assert_eq!(lines, vec!["first", "second", "third"]);
+        Ok(())
     }
 
 }
