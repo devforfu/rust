2ff8b63e6e93ee20e625a40388d7046a356d4716
devforfu <developer.z@outlook.com>
2021-07-13 22:01:23 +0200

    [WIP] Testing with random files

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -21,30 +21,58 @@ pub fn read_lines(filename: &str) -> Result<io::Lines<io::BufReader<File>>>
     Ok(io::BufReader::new(file).lines())
 }
 
+#[cfg(test)]
+mod utils {
+    use std::fs::File;
+    use rand::{rngs::OsRng, seq::SliceRandom, Rng};
+    use std::io::Write;
+
+    const WORDS: &'static [&'static str] = &include!("../res/words.json");
+    const CHARS: &[u8] = b"abcdef0123456789";
+
+    pub fn get_file_name(size: u32) -> &'static str {
+        let mut rng = OsRng;
+        let n = CHARS.len();
+        let chars = (0..size).map(|_| {
+            let index = rng.gen_range(0..n);
+            &CHARS[index]
+        });
+        let filename = chars.join("");
+        filename
+    }
+
+    pub fn create_fixed_test_file(path: &str, content: &str) {
+        let mut file = File::create(path).unwrap();
+        file.write_all(content.as_bytes()).unwrap();
+    }
+
+    pub fn create_random_test_file(path: &str, n_lines: u32, words_per_line: (u32, u32)) {
+        let (min, max) = words_per_line;
+
+        let mut rng = OsRng;
+
+        let mut file = File::create(path).unwrap();
+
+        (0..n_lines).for_each(|_| {
+            let n_words = rng.gen_range(min..max + 1);
+            let words: Vec<String> =
+                (0..n_words)
+                    .map(|_| String::from(*WORDS.choose(&mut rng).unwrap()))
+                    .collect();
+            let line: String = words.join(" ");
+            file.write_all(line.as_bytes()).unwrap();
+        });
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::sync::Once;
-    use std::io::Write;
-
-    static INIT: Once = Once::new();
-    static TEST_FILE: &str = "/tmp/test.txt";
-
-    fn init() {
-        INIT.call_once(|| {
-            create_test_file("first\nsecond\nthird");
-        });
-    }
-
-    fn create_test_file(content: &str) {
-        let mut file = File::create(TEST_FILE).unwrap();
-        file.write_all(content.as_bytes()).unwrap();
-    }
 
     #[test]
     fn test_read_lines() -> Result<()> {
-        init();
+        utils::create_fixed_test_file(TEST_FILE, "first\nsecond\nthird");
 
         let lines: Vec<String> = read_lines(TEST_FILE)?.map(|line| line.unwrap()).collect();
 
@@ -52,4 +80,8 @@ mod tests {
         Ok(())
     }
 
+    #[test]
+    fn test_find_matched_lines() {
+        utils::create_random_test_file(TEST_FILE, 10, (8, 12));
+    }
 }
