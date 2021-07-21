6fa016479bac4bc8e14298286b851b4efc9055c8
devforfu <developer.z@outlook.com>
2021-07-21 21:57:55 +0200

    Logger added

--- minigrep/src/bin/main.rs
+++ minigrep/src/bin/main.rs
@@ -1,12 +1,16 @@
+use log::info;
 use anyhow::Result;
 use structopt::StructOpt;
 use minigrep::{Cli, read_lines, Finder};
 
 fn main() -> Result<()> {
+    env_logger::init();
+
     let args: Cli = Cli::from_args();
 
     let f = Finder::new();
 
+    info!("Searching for word `{}` in file `{}`", args.pattern, args.path);
     let lines = read_lines(&args.path)?.map(|x| x.unwrap());
 
     for matched in f.find(lines, &args.pattern) {
